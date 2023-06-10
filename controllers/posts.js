1f58394c717460553cb041a76c10d8b3b0107423
Jeremy new computer <jeremy8@vt.edu>
2023-06-09 22:25:49 -0400

    added the necessary changes in the Backend to get the comments to work.

--- controllers/posts.js
+++ controllers/posts.js
@@ -71,3 +71,22 @@ export const likePost = async (req, res) => {
     res.status(404).json({ message: err.message });
   }
 };
+
+export const createComment = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId, comment } = req.body;
+    
+    const post = await Post.findById(id);
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+    
+    post.comments.push({ userId, comment });
+    const updatedPost = await post.save();
+    
+    res.status(200).json(updatedPost);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
